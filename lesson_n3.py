505ab1f07e8e2eb80b1e9e97df97fb41e63ceec6
arturka0071145 <arturka.a45@gmail.com>
2023-05-15 12:29:43 +0600

    test

---
+++ lesson_n3.py
@@ -0,0 +1,24 @@
+# rounds = 0
+# while rounds !=10:
+#     rounds +=1
+#     print(rounds)
+
+for i in range(1,1060000000+1):
+    print(i, end=' ')
+#
+# login_in_db = 'arturka.a45'
+# login = input("Введите логин:")
+# password_in_db = '2k041100'
+# password = input("Введите пароль:")
+#
+# if password_in_db == password and login_in_db == login:
+#     print("Login and password true!!!")
+# else:
+#     print("wrong!!!")
+# cash = 100
+# percent = 0.1
+# years = 5
+# for years in range(1,years+1):
+#     cash += cash * percent
+#     print((cash))
+#
